0241af81131a3e7a762f24eb7f9fa2d07c059fb6
brkagithub <mbrkic16@gmail.com>
2024-12-24 21:39:02 +0100

    Update newest hubs

--- index.cjs
+++ index.cjs
@@ -58,15 +58,15 @@ const BLOCKCHAINS = {
     testnet: {
         // This is V8 TESTNET HUB don't use this for V6
         'base:84532': {
-            hubContract: '0x3a38fC53a00C12a9883Cf8B61c8A76a88902f81C',
+            hubContract: '0x4A0aC589D7469868ff4EC889aa2c7eE43d30d823',
             rpc: 'https://sepolia.base.org',
         },
         'otp:20430': {
-            hubContract: '0x965C8970784B2c25Ca96F6D284e7E0e3AE6940d1',
+            hubContract: '0x94cb4Ce3C6ce3654b0dA47255a2325322DaD5552',
             rpc: 'https://lofar-testnet.origin-trail.network',
         },
         'gnosis:10200': {
-            hubContract: '0xC612946B0d4830F5D9f3DBD1cE0f5770183b07e1',
+            hubContract: '0xC2d165484ee044aFCD0F693a84Ae57235FD986fA',
             rpc: 'https://rpc.chiadochain.net',
             gasPriceOracleLink: 'https://blockscout.chiadochain.net/api/v1/gas-price-oracle',
         },
@@ -3076,7 +3076,7 @@ const ParanetKnowledgeMinersRegistryAbi = require$1('dkg-evm-module/abi/ParanetK
 const IdentityStorageAbi = require$1('dkg-evm-module/abi/IdentityStorage.json');
 const KnowledgeCollectionAbi = require$1('dkg-evm-module/abi/KnowledgeCollection.json');
 const KnowledgeCollectionStorageAbi = require$1('dkg-evm-module/abi/KnowledgeCollectionStorage.json');
-const AskAbi = require$1('dkg-evm-module/abi/Ask.json');
+const AskStorageAbi = require$1('dkg-evm-module/abi/AskStorage.json');
 const ChronosAbi = require$1('dkg-evm-module/abi/Chronos.json');
 
 class BlockchainServiceBase {
@@ -3100,7 +3100,7 @@ class BlockchainServiceBase {
         this.abis.IdentityStorage = IdentityStorageAbi;
         this.abis.KnowledgeCollection = KnowledgeCollectionAbi;
         this.abis.KnowledgeCollectionStorage = KnowledgeCollectionStorageAbi;
-        this.abis.Ask = AskAbi;
+        this.abis.AskStorage = AskStorageAbi;
         this.abis.Chronos = ChronosAbi;
 
         this.abis.KnowledgeCollectionStorage.filter((obj) => obj.type === 'event').forEach(
@@ -4127,7 +4127,12 @@ class BlockchainServiceBase {
     // Get ask operations
     // To get price, multiply with size in bytes and epochs
     async getStakeWeightedAverageAsk(blockchain) {
-        return this.callContractFunction('Ask', 'getStakeWeightedAverageAsk', [], blockchain);
+        return this.callContractFunction(
+            'AskStorage',
+            'getStakeWeightedAverageAsk',
+            [],
+            blockchain,
+        );
     }
 
     // Blockchain operations
